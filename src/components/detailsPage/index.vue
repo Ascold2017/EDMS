b2091a34f7e53a81b1f771b44b3f6c5e93d41ea1
Ascold2017 <ascold96@gmail.com>
2018-01-29 22:03:55 +0200

    add comments

--- src/components/detailsPage/index.vue
+++ src/components/detailsPage/index.vue
@@ -7,7 +7,7 @@
 		b-embed(
 			type="embed"
 			aspect="16by9"
-		  :src="data.document"
+			:src="data.document"
 			allowfullscreen
 			)
 		b-list-group.details__authors
@@ -18,6 +18,7 @@
 				)
 				p.subtitle {{ author.author }}
 				p.subtitle {{ author.role }}
+				p.text {{ author.comment }}
 		b-form(@submit.prevent="submitDoc").details__form
 			b-form-group(label="Выберите действие")
 				b-form-radio-group(
@@ -25,11 +26,7 @@
 					name="radioSubComponent"
 					:options="options"
 					)
-			b-form-group(
-				label="Ваш комментарий"
-				description="Укажите причину вашего отказа"
-				v-if="selected === 'reject'"
-				).details__comment
+			b-form-group(label="Ваш комментарий").details__comment
 				b-form-textarea(
 					v-model="comment"
 					placeholder="Оставьте комментарий"
@@ -42,68 +39,68 @@
 <script>
 import { mapGetters, mapActions } from "vuex";
 export default {
-  //'https://view.officeapps.live.com/op/embed.aspx?src=' + data.document
-  data() {
-    return {
-      // get id of current document
-      id: this.$router.currentRoute.params.id,
-      // current status of doc
-      selected: "",
-      options: [
-        { value: "resolve", text: "Согласен" },
-        { value: "reject", text: "НЕ согласен" }
-      ],
-      comment: "",
-      infoAlert: ""
-    };
-  },
-  computed: {
-    ...mapGetters("docsStore", ["data"]),
-    ...mapGetters("usersStore", ["token", "currentUser"])
-  },
-  methods: {
-    ...mapActions("docsStore", ["getDocumentById", "postVote"]),
-    submitDoc(e) {
-      if (this.selected) {
-        // post our vote to server
-        this.postVote({
-          id: this.id,
-          vote: this.selected,
-          comment: this.comment,
-          token: this.token,
-          author: this.currentUser
-        })
-          .then(response => {
-			this.showAlert('Ваш голос успешно добавлен!');
-			e.target.reset();
-            this.getDocumentById(this.id);
-          })
-          .catch(e => {
-			  console.log(e);
-			  this.showAlert(`Произошла ошибка: ${e}`);
-		  });
-      }
-    },
-    statusVariant(state) {
-      switch (state) {
-        case "resolve":
-          return "success";
-        case "reject":
-          return "danger";
-        case "waiting":
-          return "primary";
-        default:
-          return "warning";
-      }
-    },
-    showAlert(title) {
-      this.infoAlert = title;
-      this.$refs.alertModal.show();
-    }
-  },
-  created() {
-	this.getDocumentById(this.id);
-  }
+	//'https://view.officeapps.live.com/op/embed.aspx?src=' + data.document
+	data() {
+		return {
+			// get id of current document
+			id: this.$router.currentRoute.params.id,
+			// current status of doc
+			selected: "",
+			options: [
+				{ value: "resolve", text: "Согласен" },
+				{ value: "reject", text: "НЕ согласен" }
+			],
+			comment: "",
+			infoAlert: ""
+		};
+	},
+	computed: {
+		...mapGetters("docsStore", ["data"]),
+		...mapGetters("usersStore", ["token", "currentUser"])
+	},
+	methods: {
+		...mapActions("docsStore", ["getDocumentById", "postVote"]),
+		submitDoc(e) {
+			if (this.selected) {
+				// post our vote to server
+				this.postVote({
+					id: this.id,
+					vote: this.selected,
+					comment: this.comment,
+					token: this.token,
+					author: this.currentUser
+				})
+					.then(response => {
+						this.showAlert("Ваш голос успешно добавлен!");
+						e.target.reset();
+						this.getDocumentById(this.id);
+					})
+					.catch(e => {
+						console.log(e);
+						this.showAlert(`Произошла ошибка: ${e}`);
+					});
+			}
+		},
+		statusVariant(state) {
+			switch (state) {
+				case "resolve":
+					return "success";
+				case "reject":
+					return "danger";
+				case "waiting":
+					return "primary";
+				default:
+					return "warning";
+			}
+		},
+		showAlert(title) {
+			this.infoAlert = title;
+			this.$refs.alertModal.show();
+		}
+	},
+	created() {
+		this.getDocumentById(this.id);
+	}
 };
 </script>
 <style lang="sass" src="./style.sass" scoped></style>
